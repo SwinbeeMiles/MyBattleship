c47bf4659cc9709678f5331515cd0bfbfe17c676
TWJ <murloc2899@gmail.com>
2019-04-02 00:24:46 -0700

    Adding TileView.cs
    Tan added TileView.cs

---
+++ src (C#)/Model/TileView.cs
@@ -0,0 +1,27 @@
+﻿public enum TileView
+{
+    /// <summary>
+    ///     ''' The viewer can see sea
+    ///     ''' </summary>
+    ///     ''' <remarks>
+    ///     ''' May be masking a ship if viewed via a sea adapter
+    ///     ''' </remarks>
+    Sea,
+
+    /// <summary>
+    ///     ''' The viewer knows that site was attacked but nothing
+    ///     ''' was hit
+    ///     ''' </summary>
+    Miss,
+
+    /// <summary>
+    ///     ''' The viewer can see a ship at this site
+    ///     ''' </summary>
+    Ship,
+
+    /// <summary>
+    ///     ''' The viewer knows that the site was attacked and
+    ///     ''' something was hit
+    ///     ''' </summary>
+    Hit
+}

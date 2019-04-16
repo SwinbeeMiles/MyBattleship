9f42ef754d8206a31eb6fb2c71344d6155c40238
Toh Jie Jin <tohjiejin1999@gmail.com>
2019-04-16 13:09:53 +0800

    (JJ) Added explosion animation

--- src/UtilityFunctions.cs
+++ src/UtilityFunctions.cs
@@ -35,8 +35,8 @@ static class UtilityFunctions
 	private readonly static Color SHIP_OUTLINE_COLOR = Color.White;
 	private readonly static Color MESSAGE_COLOR = SwinGame.RGBAColor (2, 167, 252, 255);
 
-	public const int ANIMATION_CELLS = 7;
-	public const int FRAMES_PER_CELL = 8;
+	public const int ANIMATION_CELLS = 6;
+	public const int FRAMES_PER_CELL = 10;
 
 	/// <summary>
 	/// Determines if the mouse is in a given rectangle.
@@ -244,7 +244,7 @@ static class UtilityFunctions
 	/// <param name="col">Col.</param>
 	public static void AddExplosion (int row, int col)
 	{
-		AddAnimation (row, col, "Splash");
+		AddAnimation (row, col, "Explosion");
 	}
 
 	/// <summary>

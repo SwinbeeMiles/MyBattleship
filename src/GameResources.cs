bc142b43132374d2052ce818fc62af224f4bfad9
Toh Jie Jin <tohjiejin1999@gmail.com>
2019-04-18 18:21:11 +0800

    (JJ) Changed background music

--- src/GameResources.cs
+++ src/GameResources.cs
@@ -78,7 +78,7 @@ public static class GameResources
 	/// </summary>
 	private static void LoadMusic ()
 	{
-		NewMusic ("Background", "horrordrone.mp3");
+		NewMusic ("Background", "deepblue.mp3");
 	}
 
 	/// <summary>

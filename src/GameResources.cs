f22417c07bc8201cf8fad4c5f0bdc6a7d2143ff4
Tan Woon Jie <48273525+Epsilon753@users.noreply.github.com>
2019-04-29 12:31:18 +0800

    (Tan) Changed the font size for action message and fps text.

--- src/GameResources.cs
+++ src/GameResources.cs
@@ -13,9 +13,9 @@ public static class GameResources
 	/// </summary>
 	private static void LoadFonts ()
 	{
-		NewFont ("ArialLarge", "arial.ttf", 80);
-		NewFont ("Courier", "cour.ttf", 14);
-		NewFont ("CourierSmall", "cour.ttf", 8);
+		NewFont ("ArialLarge", "arial.ttf", 85);
+		NewFont ("Courier", "cour.ttf", 18);
+		NewFont ("CourierSmall", "cour.ttf", 14);
 		NewFont ("Menu", "ffaccess.ttf", 8);
 	}
 
